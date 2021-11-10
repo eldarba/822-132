315a0c6cf923e6d40d581077c8b03bd43ba2af95
Eldar <eldarba@gmail.com>
2021-11-10 21:28:11 +0200

    spring-aop-drill-solution-eldar

--- spring-aop-drill-sollution-eldar/src/main/java/app/core/aspects/Pointcuts.java
+++ spring-aop-drill-sollution-eldar/src/main/java/app/core/aspects/Pointcuts.java
@@ -2,6 +2,10 @@ package app.core.aspects;
 
 import org.aspectj.lang.annotation.Pointcut;
 
+/**
+ * a central class for general pointcuts
+ *
+ */
 public class Pointcuts {
 
 	// all methods in any class of app.core package and sub packages
