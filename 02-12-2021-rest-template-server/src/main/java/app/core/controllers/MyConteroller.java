99a31b45c5148ecea3f5ba13c03f59a5ed518cb6
jbt <jbt@192.168.0.51>
2021-12-02 21:18:11 +0200

    02-12-2021-rest-template

--- 02-12-2021-rest-template-server/src/main/java/app/core/controllers/MyConteroller.java
+++ 02-12-2021-rest-template-server/src/main/java/app/core/controllers/MyConteroller.java
@@ -1,7 +1,12 @@
 package app.core.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +15,8 @@ import app.core.entities.Person;
 @RestController
 @RequestMapping("/api")
 public class MyConteroller {
+
+	private List<Person> list = new ArrayList<>();
 
 	@GetMapping("/greet/{user}")
 	public String greet(@PathVariable String user) {
@@ -20,5 +27,15 @@ public class MyConteroller {
 	public Person getPerson() {
 		int id = (int) (Math.random() * 9999999);
 		return new Person(id, "Dan", 25);
+	}
+
+	@GetMapping("/all")
+	public List<Person> getAll() {
+		return this.list;
+	}
+
+	@PostMapping("/person")
+	public void addPerson(@RequestBody Person person) {
+		this.list.add(person);
 	}
 }
