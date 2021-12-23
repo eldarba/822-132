5254b697c5994a8d745cd9d6754409a4c5153dc3
jbt <jbt@192.168.0.51>
2021-12-23 18:32:56 +0200

    bookstore

---
+++ 06-12-21-rest-bookstore/src/main/java/app/core/entities/Book.java
@@ -0,0 +1,37 @@
+package app.core.entities;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+@ToString(exclude = "bookStore")
+@Entity
+public class Book {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String title;
+	private String author;
+	private double price;
+	private LocalDate publication;
+	@ManyToOne
+	@JoinColumn
+	private BookStore bookStore;
+
+}
---
+++ 06-12-21-rest-bookstore/src/main/java/app/core/entities/BookStore.java
@@ -0,0 +1,46 @@
+package app.core.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+@ToString(exclude = "books")
+@Entity
+public class BookStore {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	private String address;
+	@OneToMany(mappedBy = "bookStore", cascade = CascadeType.ALL)
+	private List<Book> books;
+
+	// add books to this store
+	public void add(Book... books) {
+		if (this.books == null) {
+			this.books = new ArrayList<>();
+		}
+		for (Book b : books) {
+			b.setBookStore(this);
+			this.books.add(b);
+		}
+	}
+
+}
