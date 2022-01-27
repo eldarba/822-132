f5e727a3da62fdc3ee1748ebfd0f00ca557ccd04
Eldar <eldarba@gmail.com>
2022-01-27 21:14:42 +0200

    AJAX

---
+++ 27-01-22-spring-for-ajax/src/main/java/app/core/services/ItemService.java
@@ -0,0 +1,45 @@
+package app.core.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import app.core.entities.Item;
+import app.core.repos.ItemRepo;
+
+@Service
+@Transactional
+public class ItemService {
+
+	@Autowired
+	private ItemRepo itemRepo;
+
+	public int addItem(Item item) {
+		if (itemRepo.existsById(item.getId())) {
+			throw new RuntimeException("addItem failed - item already exists");
+		}
+		return this.itemRepo.save(item).getId();
+	}
+
+	public Item getItem(int id) {
+		return itemRepo.findById(id).orElseThrow();
+	}
+
+	public List<Item> getAllItems() {
+		return itemRepo.findAll();
+	}
+
+	public void updateItem(Item item) {
+		if (itemRepo.existsById(item.getId())) {
+			this.itemRepo.save(item);
+		}
+		throw new RuntimeException("updateItem failed - item not found");
+	}
+
+	public void deleteItem(int id) {
+		itemRepo.deleteById(id);
+	}
+
+}
