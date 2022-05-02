ee5d4fb8af4bcc94f8eb5192ab688af7edd5467c
Eldar <eldarba@gmail.com>
2022-05-02 21:46:50 +0300

    micro services

--- spring-for-angular-products-login-filters-822.132-eldar/src/main/java/app/core/controllers/LoginController.java
+++ spring-for-angular-products-login-filters-822.132-eldar/src/main/java/app/core/controllers/LoginController.java
@@ -3,7 +3,6 @@ package app.core.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,7 +12,7 @@ import org.springframework.web.server.ResponseStatusException;
 import app.core.exceptions.ProductException;
 import app.core.services.LoginService;
 
-@CrossOrigin
+//@CrossOrigin
 @RestController
 @RequestMapping("/login")
 public class LoginController {
--- spring-for-angular-products-login-filters-822.132-eldar/src/main/java/app/core/controllers/ProductController.java
+++ spring-for-angular-products-login-filters-822.132-eldar/src/main/java/app/core/controllers/ProductController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,7 +19,7 @@ import app.core.entities.Product;
 import app.core.exceptions.ProductException;
 import app.core.services.ProductService;
 
-@CrossOrigin
+// @CrossOrigin
 @RestController
 @RequestMapping("/api/product")
 public class ProductController {
